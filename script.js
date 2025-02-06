f3b13e48c20e7b374deb07b536b25283d207455b
MenukaSankalpa <menuka.sankalpa@gmail.com>
2025-02-06 16:28:51 +0530

    createPassword function improve in js file

--- script.js
+++ script.js
@@ -5,3 +5,18 @@ const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 const lowerCase = "abcdefghijklmnopqrstuvwxyz";
 const numbers = "0123456789";
 const symbols = "!@#$%^&*()_-+=[]{}:;<>.,?/|";
+
+function createPassword() {
+  let password = "";
+  password += upperCase[Math.floor(Math.random() * upperCase.length)];
+  password += lowerCase[Math.floor(Math.random() * lowerCase.length)];
+  password += number[Math.floor(Math.random() * number.length)];
+  password += symbol[Math.floor(Math.random() * symbol.length)];
+
+  const allChars = upperCase + lowerCase + numbers + symbols;
+
+  while (length > password.length) {
+    password += allChars[Math.floor(Math.random() * allChars.length)];
+  }
+  passwordBox.value = password;
+}
