cf242c7446eeb7e103e4fe1e56fe9d5af42f01b1
MenukaSankalpa <menuka.sankalpa@gmail.com>
2025-02-06 16:41:38 +0530

    copy function improve in js file

--- script.js
+++ script.js
@@ -10,8 +10,8 @@ function createPassword() {
   let password = "";
   password += upperCase[Math.floor(Math.random() * upperCase.length)];
   password += lowerCase[Math.floor(Math.random() * lowerCase.length)];
-  password += number[Math.floor(Math.random() * number.length)];
-  password += symbol[Math.floor(Math.random() * symbol.length)];
+  password += numbers[Math.floor(Math.random() * numbers.length)];
+  password += symbols[Math.floor(Math.random() * symbols.length)];
 
   const allChars = upperCase + lowerCase + numbers + symbols;
 
@@ -19,4 +19,15 @@ function createPassword() {
     password += allChars[Math.floor(Math.random() * allChars.length)];
   }
   passwordBox.value = password;
+}
+
+function copyPassword() {
+  navigator.clipboard
+    .writeText(passwordBox.value)
+    .then(() => {
+      alert("Password copied to clipboard!");
+    })
+    .catch((err) => {
+      console.error("Failed to copy text: ", err);
+    });
 }
