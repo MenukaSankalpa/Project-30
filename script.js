aa543669aa762b590f172010688cb84c5e2b9336
MenukaSankalpa <menuka.sankalpa@gmail.com>
2025-02-06 16:19:58 +0530

    add all generate types

---
+++ script.js
@@ -0,0 +1,7 @@
+const passwordBox = document.getElementById("password");
+const length = 12;
+
+const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const lowerCase = "abcdefghijklmnopqrstuvwxyz";
+const numbers = "0123456789";
+const symbols = "!@#$%^&*()_-+=[]{}:;<>.,?/|";
